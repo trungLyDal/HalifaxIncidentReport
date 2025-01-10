d8ea8a52189c4615839044c7fa90789822f4ba26
Trung Ly <Jun.Ly@dal.ca>
2025-01-09 21:11:26 -0400

    made hamburger menu, landing page

--- incident-map/src/App.js
+++ incident-map/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './header'
 import React from 'react';
 import MapComponent from './MapComponent';
+import Landing from './LandingPage';
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <Landing/>
       <MapComponent />
     </div>
   );
--- incident-map/src/header.js
+++ incident-map/src/header.js
@@ -1,19 +1,41 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const header = () => {
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+
+    // Lock scroll when menu is open
+    if (!menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  };
+
   return (
     <div className="header-home">
       <h1>HIT</h1>
-      {/* <ul className = "homepage-links">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Resources</li>
-      </ul> */}
+      <button className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        ☰
+      </button>
+
+      {/* Sidebar Menu */}
+      <div className={`sidebar ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+
+      {/* Overlay */}
+      <div className={`sidebar-overlay ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}></div>
     </div>
   );
 };
 
-export default header;
+export default Header;
